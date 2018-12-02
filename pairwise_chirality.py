10d9f2b0c1f1a3eea4763ec8136f933428b3c1b1
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2018-12-02 14:21:43 +1100

    added docstrings for pairwise_chirality functions

--- pairwise_chirality.py
+++ pairwise_chirality.py
@@ -17,6 +17,7 @@ from scipy.optimize import minimize as _minimize
 
 
 def _template_stack(template, nrotations=10):
+    """ Returns 3d array of nrotations images rotated through full circle. """
     angles = _np.linspace(0, 360, nrotations)
     
     ## imrotate does a weird re-scaling of the data, necessary for later comparison
@@ -29,6 +30,7 @@ def _template_stack(template, nrotations=10):
 
 
 def _template_compare(template1, template2, x, y, r):
+    """ Returns normalized pixel-to-pixel comparison of two image arrays with relative angle r. """
     _, h, w = template2.shape
     I2d = _interp2d(range(h), range(w), template2[0,:,:])
     t1 = _imrotate(template1[0,:,:], r)
@@ -36,6 +38,7 @@ def _template_compare(template1, template2, x, y, r):
     return 1 - _np.sum(_np.abs(t1 - t2))/_np.sum(_np.abs(t1)+_np.abs(t2))
 
 def _template_compare_fixed_r(template1, template2, x, y, r_index):
+    """  Returns normalized pixel-to-pixel comparison of two image arrays with fixed relative angle r_index. """
     nrotations = template1.shape[0]
     r_index = int(r_index)
     if r_index >= nrotations:
@@ -48,6 +51,7 @@ def _template_compare_fixed_r(template1, template2, x, y, r_index):
 
 
 def _compare_templates(template, template2):
+    """ Returns highest correlation between two pre-rotated image stacks. """
     nrotations = template.shape[0]
 
     ## optimization with imrotate
@@ -75,6 +79,7 @@ def _compare_templates(template, template2):
 
 
 def sort_chirality(templates, sorted_labels, nrotations=10, category_indexes=None):
+    """ Returns list of chiral sorted contour index labels. """
     new_labels = [ii for ii in sorted_labels]
     templates = _np.array(templates)
     

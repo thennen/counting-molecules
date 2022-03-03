03b4eb6711d9c88034997bc775ad15f4e30cb898
T Hennen <tyler@hennen.us>
2022-03-03 13:42:42 +0100

    update version string

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = '0'
+version = '0.1.0'
 
 setup(
     name='counting_molecules',
